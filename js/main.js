7124a9e20950cb27d8752df637e9af6c04f23b3a
WindWT <leibao0723@gmail.com>
2016-11-12 18:20:30 +0800

    first working edition!

--- js/main.js
+++ js/main.js
@@ -1,13 +1,61 @@
 $(function () {
-    function setUnit(unit) {}
+
+    var formatStory = function (story) {
+        return formatRichText(story);
+    };
+    var formatRichText = function (richText) {
+        return richText.replace(/(?:\r\n|\r|\n|\\n)/g, "<br/>").replace(/\[-\]/g, "</span>").replace(/\[([A-Za-z0-9]{6})\]/g, "<span style='color:#$1'>");
+    };
+
+    function setUnit(unit) {
+        $('#story').tab('show');
+
+        $('#txtID').val(unit.id);
+        $('#txtGID').val(unit.gId);
+        $('#icon').attr("src", getIcon(unit.gId));
+
+        $('#divNameJP').text(unit.name);
+
+        $('#divStoryJP').html(formatStory(unit.story));
+        $('#txtStory').val(unit.story);
+        $('#txtStory').height($('#divStoryJP').parent().height());
+
+        $('#divCutinJP').empty();
+        $.each(unit.cutin, function (i, o) {
+            $('#divCutinJP').append($("<li>").text(o));
+        });
+
+        if (unit.accessory) {
+            $('#tabHeaderAccessory').show();
+            $('#divAccessoryNameJP').text(unit.accessory.name);
+            $('#divAccessoryDetailJP').html(formatStory(unit.accessory.detail));
+            $('#txtAccessoryDetail').val(unit.accessory.detail);
+        } else {
+            $('#tabHeaderAccessory').hide();
+        }
+
+    }
 
     function getUnit() {
-        var unit = {};
-        return unit;
+        return {
+            id: $('#txtID').val(),
+            gId: $('#txtGID').val(),
+            name: $('#txtName').val(),
+            story: $('#txtStory').val(),
+            cutin: $('#txtCutin').val().split("\n"),
+            accessory: {
+                name: $('#txtAccessoryName').val(),
+                detail: $('#txtAccessoryDetail').val()
+            }
+        };
     }
 
     function getApi(operate) {
         return 'http://nodejs-rodora.rhcloud.com/' + operate;
+    }
+
+    function getIcon(gId) {
+        return 'https://rodora.github.io/img/Icon/I' + gId + '.png'
     }
 
     function initRouter() {
@@ -28,44 +76,52 @@ $(function () {
         var app_router = new AppRouter;
 
         app_router.on('route:unitDetailRoute', function (id) {
-            $.get({
-                    url: getApi('unit'),
-                    data: {
-                        id: id
-                    },
-                    dataType: 'json'
-                })
+            $.get(getApi('unit/id/' + id))
                 .done(function (unit) {
-                    setunit(unit);
+                    setUnit(unit);
                 });
         });
         app_router.on('route:unitDetailByGIdRoute', function (gid) {
-            $.get({
-                    url: getApi('unit'),
-                    data: {
-                        gid: gid
-                    },
-                    dataType: 'json'
-                })
+            $.get(getApi('unit/gid/' + gid))
                 .done(function (unit) {
-                    setunit(unit);
+                    setUnit(unit);
                 });
         });
         app_router.on('route:randomRoute', function (lang) {
-            $.get({
-                    url: getApi('random')
-                })
-                .done(function (id) {
-                    app_router.navigate("unit/id/" + id);
+            $.get(getApi('random'))
+                .done(function (unit) {
+                    app_router.navigate("unit/id/" + unit.id);
+                    setUnit(unit);
                 });
-
-            //location.reload();
         });
 
         Backbone.history.start();
     }
 
+    function initControl() {
+        $('#txtStory').change(function (event) {
+            $('#divStoryJP').html(formatStory($('#txtStory').val()));
+            $('#txtStory').height($('#divStoryJP').parent().height());
+        });
+
+        $('#btnSubmit').click(function () {
+            $.post(getApi('submit'), {
+                    data: JSON.stringify(getUnit())
+                })
+                .done(function (unit) {
+                    location.href = "#random";
+                });
+        });
+    }
+
     var init = function () {
+        $(document).ajaxStart(function () {
+            NProgress.start()
+        });
+        $(document).ajaxComplete(function () {
+            NProgress.done()
+        });
         initRouter();
+        initControl();
     }();
 });
